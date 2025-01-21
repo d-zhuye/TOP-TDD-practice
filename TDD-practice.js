70a9ce454145cfd149ba5457335128d4b05a1a68
d-zhuye <180813277+d-zhuye@users.noreply.github.com>
2025-01-21 01:33:39 -0800

    Add reverseString function and unit tests

--- TDD-practice.js
+++ TDD-practice.js
@@ -2,6 +2,13 @@ const practiceModule = {
   capitalize(str) {
     if (str === "" || !str) return str;
     return str[0].toUpperCase() + str.slice(1);
+  },
+
+  reverseString(str) {
+    if (typeof str !== "string") throw new Error("Invalid argument. Enter a string.");
+    if (!str) return str;
+
+    return str.split("").reverse().join("");
   }
 }
 
--- TDD-practice.test.js
+++ TDD-practice.test.js
@@ -1,3 +1,4 @@
+import exp from "constants";
 import { practiceModule } from "./TDD-practice";
 
 describe("First character capitalized in string", () => {
@@ -32,3 +33,39 @@ describe("First character capitalized in string", () => {
   })
 });
 
+
+describe("takes a string and returns it reversed", () => {
+  test("function exists", () => {
+    expect(practiceModule.reverseString).toBeDefined();
+  })
+
+  test("returns a reversed string", () => {
+    expect(practiceModule.reverseString("orange cat")).toBe("tac egnaro");
+  })
+
+  test("returns another string reversed", () => {
+    const str = "mango the orange cat";
+    const reversedStr = "tac egnaro eht ognam";
+    expect(practiceModule.reverseString(str)).toBe(reversedStr);
+  })
+
+  test("returns string with number and symbols reversed", () => {
+    expect(practiceModule.reverseString("0r@nge c@t")).toBe("t@c egn@r0");
+  })
+
+  test("returns empty string", () => {
+    expect(practiceModule.reverseString("")).toBe("");
+  })
+
+  test("returns white space string", () => {
+    expect(practiceModule.reverseString("   ")).toBe("   ");
+  })
+
+  test("return a string with unicode characters", () => {
+    expect(practiceModule.reverseString("你好世界")).toBe("界世好你");
+  })
+
+  test("throw error for non-string arguments", () => {
+    expect(() => practiceModule.reverseString(123)).toThrow("Invalid argument. Enter a string.");
+  })
+})
